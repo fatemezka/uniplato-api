c0048ed780cc48b6fd6f864089aeb1c2932c3a65
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-12-22 01:42:41 +0330

    add user database services

---
+++ src/services/user.ts
@@ -0,0 +1,28 @@
+import prisma from "../database/prisma";
+
+export const create = async (
+  name: string,
+  family: string,
+  username: string,
+  email: string,
+  password: string
+) => {
+  try {
+    return await prisma.user.create({
+      data: { name, family, username, email, password },
+      select: {
+        id: true,
+        name: true,
+        family: true,
+        username: true,
+        email: true,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getByEmail = async (email: string) => {
+  return await prisma.user.findUnique({ where: { email } });
+};
