3acf54d3e6fe440b69e4aa992491309b12d81933
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-12-24 13:42:19 +0330

    remove useless comments

--- src/app.ts
+++ src/app.ts
@@ -1,84 +1,10 @@
-import Fastify, { FastifyListenOptions } from "fastify";
-// import fastifyJwt from "@fastify/jwt";
-// import fastifySwagger from "@fastify/swagger";
-// import fastifySwaggerUI from "@fastify/swagger-ui";
-// import { userRoutes } from "./routes/user";
-// import { categoryRoutes } from "./routes/category";
+import { FastifyListenOptions } from "fastify";
 import logger from "./logger";
 import { createServer } from "./server";
 
 // dotenv
 import dotenv from "dotenv";
 dotenv.config();
-
-// const server = Fastify();
-
-// async function main() {
-//   const port: number = parseInt(<string>process.env.SERVER_PORT, 10) || 3000;
-
-//   // jwt
-//   await server.register<any>(fastifyJwt, {
-//     secret: process.env.JWT_SECRET,
-//   });
-
-//   // swagger
-//   await server.register<any>(fastifySwagger, {
-//     swagger: {
-//       info: {
-//         title: "Uniplato Swagger API",
-//         description: "Testing the Uniplato project APIs.",
-//       },
-//       schemes: ["http"],
-//       securityDefinitions: {
-//         apiKey: {
-//           type: "apiKey",
-//           name: "x-uniplato-auth",
-//           in: "header",
-//         },
-//       },
-//     },
-//   });
-
-//   await server.register(fastifySwaggerUI, {
-//     routePrefix: "/",
-//     uiConfig: {
-//       docExpansion: "full",
-//       deepLinking: false,
-//     },
-//     uiHooks: {
-//       onRequest: function (request, reply, next) {
-//         next();
-//       },
-//       preHandler: function (request, reply, next) {
-//         next();
-//       },
-//     },
-//     staticCSP: true,
-//     transformStaticCSP: (header) => header,
-//     transformSpecification: (swaggerObject, request, reply) => {
-//       return swaggerObject;
-//     },
-//     transformSpecificationClone: true,
-//   });
-
-//   // routes
-//   server.register(userRoutes, { prefix: "/user" });
-//   server.register(categoryRoutes, { prefix: "/category" });
-
-//   // run server
-//   try {
-//     let server_options: FastifyListenOptions = { port };
-//     await server.listen(server_options);
-//     logger.info(`Server is ready on port: ${port}`);
-//   } catch (error) {
-//     logger.error((error as Error).message);
-//     process.exit(1);
-//   }
-// }
-
-// export { server };
-
-// main();
 
 export const server = createServer();
 const startServer = async () => {
