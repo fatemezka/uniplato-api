830807692a2dc630fc090b67cd6cfaea4bc1f5d0
fateme zahra <fatemezahrakalantarii@gmail.com>
2023-12-25 04:17:42 +0330

    complete user & category tests

--- test/category.test.ts
+++ test/category.test.ts
@@ -1,6 +1,8 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
+import { v4 as uuidv4 } from "uuid";
 import { server } from "../src/app";
+import prisma from "../src/database/prisma";
 
 // Assertion Style
 chai.should();
@@ -8,59 +10,159 @@ chai.use(chaiHttp);
 
 const httpServer = server.server;
 
+// `register` and `login` endpoint is calling here, too.
+// because of authentication for other endpoints.
 describe("Category Endpoints tests", () => {
-  describe("Login and Get /category/all", () => {
-    it("It should GET all categories", (done) => {
-      chai
-        .request(httpServer)
-        .post("/user/login")
-        .send({
-          email: "mail@green.com",
-          password: "1234",
-        })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          let access_token = res.body.access_token;
-          access_token.should.be.a("string");
+  let access_token: string;
+  let score = 100;
+  let testing_user = {
+    name: "Test Name",
+    family: "Test Family",
+    username: "test_username",
+    email: "mail@test.com",
+    password: "1234",
+  };
+  let testing_category = {
+    id: uuidv4(),
+    title: "Test Category",
+    score,
+    createdAt: new Date(),
+  };
 
-          chai
-            .request(httpServer)
-            .get("/category/all")
-            .set("x-uniplato-auth", access_token)
-            .end((err, res) => {
-              res.should.have.status(200);
-              res.body.should.be.a("array");
-            });
-
-          done();
-        });
+  // initial database
+  before(async () => {
+    // delete test user if exists
+    await prisma.user.deleteMany({
+      where: {
+        OR: [
+          { username: testing_user.username },
+          { email: testing_user.email },
+        ],
+      },
     });
 
-    it("It should NOT GET all categories", (done) => {
-      chai
-        .request(httpServer)
-        .post("/user/login")
-        .send({
-          email: "mail@green.com",
-          password: "1234",
-        })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          let access_token = res.body.access_token;
-          access_token.should.be.a("string");
-
-          chai
-            .request(httpServer)
-            .get("/categoryy/all")
-            .set("x-uniplato-auth", access_token)
-            .end((err, res) => {
-              res.should.have.status(404);
-            });
-
-          done();
-        });
+    // delete test category if exists
+    await prisma.category.deleteMany({
+      where: { title: testing_category.title },
     });
+
+    // create test category
+    await prisma.category.create({
+      data: testing_category,
+    });
+  });
+
+  // close httpServer`
+  after(async () => {
+    await httpServer.close();
+  });
+
+  // 404 test
+  it("It should Return 404 error!", async () => {
+    let response = await chai.request(httpServer).get("/wrong");
+    response.should.have.status(404);
+  });
+
+  // Register (create testing user)
+  it("It should Register a user", async () => {
+    let response = await chai
+      .request(httpServer)
+      .post("/user/register")
+      .send(testing_user);
+    response.should.have.status(201);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id");
+    response.body.should.have.property("name").eq(testing_user.name);
+    response.body.should.have.property("family").eq(testing_user.family);
+    response.body.should.have.property("email").eq(testing_user.email);
+    response.body.should.have.property("username").eq(testing_user.username);
+  });
+
+  // Login user (to initial access_token)
+  it("It should Login a user", async () => {
+    let response = await chai.request(httpServer).post("/user/login").send({
+      email: testing_user.email,
+      password: testing_user.password,
+    });
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("access_token");
+    access_token = response.body.access_token;
+    access_token.should.be.a("string");
+  });
+
+  // 1. Get all categories
+  it("It should GET all categories", async () => {
+    let response = await chai
+      .request(httpServer)
+      .get("/category/all")
+      .set("x-uniplato-auth", access_token);
+    response.should.have.status(200);
+    response.body.should.be.a("array");
+  });
+
+  // 2. Get a category by ID
+  it("It should GET a category detail", async () => {
+    let response = await chai
+      .request(httpServer)
+      .get(`/category/${testing_category.id}`)
+      .set("x-uniplato-auth", access_token);
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id").eq(testing_category.id);
+    response.body.should.have.property("title").eq(testing_category.title);
+    response.body.should.have.property("score").eq(score);
+  });
+
+  // 3. Update score of a category by ID
+  it("It should Update score of category", async () => {
+    let updated_score = 100;
+    let response = await chai
+      .request(httpServer)
+      .put(`/category/${testing_category.id}`)
+      .set("x-uniplato-auth", access_token)
+      .send({
+        score: updated_score,
+      });
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id").eq(testing_category.id);
+    response.body.should.have.property("title").eq(testing_category.title);
+    response.body.should.have.property("score").eq(updated_score);
+    score = updated_score;
+  });
+
+  // 4. Increase score of a category by ID
+  it("It should Increase score of category", async () => {
+    let response = await chai
+      .request(httpServer)
+      .put(`/category/${testing_category.id}`)
+      .set("x-uniplato-auth", access_token)
+      .send({
+        operation: "increase",
+      });
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id").eq(testing_category.id);
+    response.body.should.have.property("title").eq(testing_category.title);
+    response.body.should.have.property("score").eq(score + 1);
+    score += 1;
+  });
+
+  // 5. Decrease score of a category by ID
+  it("It should Decrease score of category", async () => {
+    let response = await chai
+      .request(httpServer)
+      .put(`/category/${testing_category.id}`)
+      .set("x-uniplato-auth", access_token)
+      .send({
+        operation: "decrease",
+      });
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id").eq(testing_category.id);
+    response.body.should.have.property("title").eq(testing_category.title);
+    response.body.should.have.property("score").eq(score - 1);
+    score -= score;
   });
 });
--- test/user.test.ts
+++ test/user.test.ts
@@ -11,77 +11,60 @@ const httpServer = server.server;
 
 describe("User Endpoints tests", () => {
   let testing_user = {
-    name: "Alex",
+    name: "Alex (test user)",
     family: "Green",
     username: "alex_green",
     email: "mail@green.com",
     password: "1234",
   };
 
-  describe("User Register", () => {
-    before(async () => {
-      // delete user by test's user email or username
-      await prisma.user.deleteMany({
-        where: {
-          OR: [
-            { username: testing_user.username },
-            { email: testing_user.email },
-          ],
-        },
-      });
-    });
-
-    it("It should Register a user", (done) => {
-      chai
-        .request(httpServer)
-        .post("/user/register")
-        .send(testing_user)
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.be.a("object");
-          res.body.should.have.property("id");
-          res.body.should.have.property("name");
-          res.body.should.have.property("family");
-          res.body.should.have.property("username");
-
-          done();
-        });
+  before(async () => {
+    // delete user by test's user email or username
+    await prisma.user.deleteMany({
+      where: {
+        OR: [
+          { username: testing_user.username },
+          { email: testing_user.email },
+        ],
+      },
     });
   });
 
-  describe("User Login", () => {
-    before(async () => {
-      // delete user by test's user email or username
-      await prisma.user.deleteMany({
-        where: {
-          OR: [
-            { username: testing_user.username },
-            { email: testing_user.email },
-          ],
-        },
-      });
+  after(async () => {
+    await httpServer.close();
+  });
 
-      // create new user by testing_user info
-      await prisma.user.create({ data: testing_user });
+  // 1. Test 404
+  it("It should Return 404 error!", async () => {
+    let response = await chai.request(httpServer).get("/wrong");
+    response.should.have.status(404);
+  });
+
+  // 2. Test Register
+  it("It should Register a user", async () => {
+    let response = await chai
+      .request(httpServer)
+      .post("/user/register")
+      .send(testing_user);
+    response.should.have.status(201);
+    response.body.should.be.a("object");
+    response.body.should.have.property("id");
+    response.body.should.have.property("name").eq(testing_user.name);
+    response.body.should.have.property("family").eq(testing_user.family);
+    response.body.should.have.property("email").eq(testing_user.email);
+    response.body.should.have.property("username").eq(testing_user.username);
+  });
+
+  // 3. Test Login
+  it("It should Login a user", async () => {
+    let response = await chai.request(httpServer).post("/user/login").send({
+      email: testing_user.email,
+      password: testing_user.password,
     });
-
-    it("It should Login a user", (done) => {
-      chai
-        .request(httpServer)
-        .post("/user/login")
-        .send({
-          email: testing_user.email,
-          password: testing_user.password,
-        })
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("access_token");
-          let access_token = res.body.access_token;
-          access_token.should.be.a("string");
-
-          done();
-        });
-    });
+    response.should.have.status(200);
+    response.body.should.be.a("object");
+    response.body.should.have.property("access_token");
+    let access_token = response.body.access_token;
+    access_token.should.be.a("string");
   });
 });
